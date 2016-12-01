88080d3120d951acadcdb67cba75af63c81f32b5
nkhaja <nbl.khaja@gmail.com>
2016-11-30 22:08:40 -0800

    finished hashtable

--- hashtable.py
+++ hashtable.py
@@ -13,41 +13,92 @@ class HashTable(object):
         """Return a string representation of this hash table"""
         return 'HashTable({})'.format(self.length())
 
+
     def _bucket_index(self, key):
         """Return the bucket index where the given key would be stored"""
         return hash(key) % len(self.buckets)
 
     def length(self):
         """Return the length of this hash table by traversing its buckets"""
+        total = 0
+        for b in self.buckets:
+            total += b.length()
+        return total
         # TODO: Count number of key-value entries in each of the buckets
-        pass
 
-    def contains(self):
+    def contains(self, key):
         """Return True if this hash table contains the given key, or False"""
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        for i in bucket:
+            if i.data[0] == key:
+                return True
+        return False
+
         # TODO: Check if the given key exists in a bucket
-        pass
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError"""
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        for i in bucket:
+            if i.data[0] == key:
+                return i.data[1]
+        raise KeyError
+
+
         # TODO: Check if the given key exists and return its associated value
-        pass
+
 
     def set(self, key, value):
         """Insert or update the given key with its associated value"""
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        entry = (key, value)
+        reset = None
+        for i in bucket:
+            if i.data[0] == key:
+                i.data = entry
+                return
+        bucket.append(entry)
+
+
         # TODO: Insert or update the given key-value entry into a bucket
-        pass
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError"""
+        index = self._bucket_index(key)
+        bucket = self.buckets[index]
+        toDelete = None
+        for i in bucket:
+            if key == i.data[0]:
+                toDelete = i
+                break
+
+        if toDelete:
+            bucket.delete(toDelete.data)
+        else:
+            raise KeyError
+
         # TODO: Find the given key and delete its entry if found
-        pass
 
     def keys(self):
         """Return a list of all keys in this hash table"""
+        allKeys = []
+        for b in self.buckets:
+            for i in b:
+                allKeys.append(i.data[0])
+        return allKeys
+
+
         # TODO: Collect all keys in each of the buckets
-        pass
 
     def values(self):
         """Return a list of all values in this hash table"""
+        allValues = []
+        for b in self.buckets:
+            for i in b:
+                allValues.append(i.data[1])
+        return allValues
         # TODO: Collect all values in each of the buckets
         pass
---
+++ linkedlist.py
@@ -0,0 +1,164 @@
+#!python
+
+from __future__ import print_function
+
+
+class Node(object):
+
+    def __init__(self, data):
+        """Initialize this node with the given data"""
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        """Return a string representation of this node"""
+        return 'Node({})'.format(repr(self.data))
+
+
+class LinkedList(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this linked list; append the given items, if any"""
+        self.head = None
+        self.tail = None
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def __repr__(self):
+        """Return a string representation of this linked list"""
+        return 'LinkedList({})'.format(self.as_list())
+
+    def as_list(self):
+        """Return a list of all items in this linked list"""
+        result = []
+        current = self.head
+        while current != None:
+            result.append(current.data)
+            current = current.next
+            # print('loop one')
+        return result
+
+    def is_empty(self):
+        """Return True if this linked list is empty, or False"""
+        return self.head is None
+
+    def length(self):
+        """Return the length of this linked list by traversing its nodes"""
+
+        if self.is_empty():
+            return 0
+
+        count = 1
+        current_node = self.head
+        while current_node.next != None:
+            count += 1
+            current_node = current_node.next
+            # print('loop two')
+        return count
+
+
+    def append(self, item):
+        """Insert the given item at the tail of this linked list"""
+        new_node = Node(item)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+
+
+    def prepend(self, item):
+        """Insert the given item at the head of this linked list"""
+
+        if self.head == None:
+            new_node =  Node(item)
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node = Node(item)
+            new_node.next = self.head
+            self.head = new_node
+
+
+    def delete(self, item):
+        """Delete the given item from this linked list, or raise ValueError"""
+        current_node = self.head
+        previous_node = None
+
+        if self.is_empty():
+            raise ValueError
+
+        if (self.head.data == item or self.head.data is item) and self.length() == 1:
+            self.head = None
+            self.tail = None
+
+        while current_node is not None:
+            if current_node.data == item or current_node.data is item:
+                #check if its the head
+                if previous_node is None:
+                    self.head = current_node.next
+                    current_node.next = None
+                    return
+                #check if its the tail
+                elif current_node.next is None:
+                    previous_node.next = None
+                    self.tail = previous_node
+                    return
+                #Somewhere in the middle
+                else:
+                    previous_node.next = current_node.next
+                    current_node.next = None
+                    return
+            previous_node = current_node
+            current_node = current_node.next
+        print("using latest file")
+        raise ValueError
+
+
+
+    def find(self, quality):
+        """Return an item from this linked list satisfying the given quality"""
+        # TODO: find item where quality(item) is True
+
+        current_node = self.head
+        while current_node is not None:
+            if quality(current_node.data):
+                return current_node.data
+            current_node = current_node.next
+        return None
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.next
+
+
+def test_linked_list():
+    ll = LinkedList()
+    print(ll)
+    ll.append('A')
+    print(ll)
+    ll.append('B')
+    print(ll)
+    ll.append('C')
+    print(ll)
+    print('head: ' + str(ll.head))
+    print('tail: ' + str(ll.tail))
+    print(ll.length())
+
+    ll.delete('A')
+    print(ll)
+    ll.delete('C')
+    print(ll)
+    ll.delete('B')
+    print(ll)
+    print(ll.length())
+
+
+if __name__ == '__main__':
+    test_linked_list()
